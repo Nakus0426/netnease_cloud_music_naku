c414ee78ad5b6e935e2a0e713db21ec32ac5f144
Nakus0426 <820774459@qq.com>
2025-05-26 21:46:21 +0800

    build: 更新签名配置和项目设置
    - 移除 BuildProfile.ets 中的冗余变量导出
    - 更新 .gitignore 文件，添加 sign.json 到忽略列表- 清空 build-profile.json5 中的签名配置
    - 新增 hvigorfile.ts 配置文件，引入签名配置和版本信息

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,39 @@
-import { appTasks } from '@ohos/hvigor-ohos-plugin';
+import { appTasks } from '@ohos/hvigor-ohos-plugin'
+import * as sign from './sign.json'
 
 export default {
   system: appTasks,
-  plugins: []
+  plugins: [],
+  ohos: {
+    overrides: {
+      signingConfig: getSigningConfig(), //签名配置对象
+      appOpt: {
+        versionCode: getVersionCode(),
+        versionName: getVersionName(),
+      }
+    }
+  }
+}
+
+function getSigningConfig() {
+  return {
+    type: 'HarmonyOS',
+    material: {
+      certpath: sign.certpath,
+      storePassword: sign.storePassword,
+      keyAlias: sign.keyAlias,
+      keyPassword: sign.keyPassword,
+      profile: sign.profile,
+      signAlg: sign.signAlg,
+      storeFile: sign.storeFile
+    }
+  }
+}
+
+function getVersionCode() {
+  return 100000
+}
+
+function getVersionName() {
+  return '1'
 }
